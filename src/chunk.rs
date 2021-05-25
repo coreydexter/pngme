46eb2155f0da7158db7b6145f698adf91095930b
Corey Dexter <coreyjdexter@gmail.com>
2021-05-25 22:00:22 +1000

    Implement the Chunk struct

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,8 +1,31 @@
 use std::convert::TryFrom;
 use std::fmt::Display;
+use std::io::Read;
 
 use crate::chunk_type::ChunkType;
 use crate::{Error, Result};
+
+#[derive(Debug)]
+enum ChunkError {
+    InvalidCRCValue,
+    RemainingBytes,
+    LengthTooLarge,
+}
+
+impl std::error::Error for ChunkError {}
+impl std::fmt::Display for ChunkError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ChunkError::InvalidCRCValue => {
+                write!(f, "Provided CRC value does not match calculated CRC value")
+            }
+            ChunkError::LengthTooLarge => write!(f, "Length exceeds allow range"),
+            ChunkError::RemainingBytes => {
+                write!(f, "There were bytes remaining, length is likely incorrect")
+            }
+        }
+    }
+}
 
 pub struct Chunk {
     // By the PNG 1.2 specification length must be less than
@@ -16,23 +39,26 @@ pub struct Chunk {
 
 impl Chunk {
     pub fn length(&self) -> u32 {
-        unimplemented!("length")
+        self.length
     }
 
     pub fn chunk_type(&self) -> &ChunkType {
-        unimplemented!("chunk_type")
+        &self.chunk_type
     }
 
     pub fn data(&self) -> &[u8] {
-        unimplemented!("data")
+        &self.chunk_data[..]
     }
 
     pub fn crc(&self) -> u32 {
-        unimplemented!("crc")
+        self.crc
     }
 
     pub fn data_as_string(&self) -> Result<String> {
-        unimplemented!("data_as_string")
+        let str = String::from_utf8(self.chunk_data.clone())
+            .expect("ahh whatever failed to parse utf-8 bytes as a string");
+
+        Ok(str)
     }
 
     pub fn as_bytes(&self) -> Vec<u8> {
@@ -42,7 +68,11 @@ impl Chunk {
 
 impl Display for Chunk {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        unimplemented!("display")
+        write!(
+            f,
+            "{}",
+            self.data_as_string().expect("ahh todo convert error type")
+        )
     }
 }
 
@@ -50,8 +80,79 @@ impl TryFrom<&[u8]> for Chunk {
     type Error = Error;
 
     fn try_from(value: &[u8]) -> Result<Self> {
-        unimplemented!("try from &[u8]")
+        let orig_value = value;
+        let mut value = value;
+
+        let mut length = [0 as u8; 4];
+        value.read_exact(&mut length)?;
+
+        let length = u32::from_be_bytes(length);
+
+        if length > (1 << 31) {
+            return Err(Box::new(ChunkError::LengthTooLarge));
+        }
+
+        let mut chunk_type_buf = [0 as u8; 4];
+        value.read_exact(&mut chunk_type_buf)?;
+        let chunk_type = ChunkType::try_from(chunk_type_buf)?;
+
+        let mut chunk_data: Vec<u8> = vec![0; length as usize];
+        value.read_exact(&mut chunk_data)?;
+
+        let mut crc = [0 as u8; 4];
+        value.read_exact(&mut crc)?;
+        let crc = u32::from_be_bytes(crc);
+
+        if !value.is_empty() {
+            return Err(Box::new(ChunkError::RemainingBytes));
+        }
+
+        // The CRC is calculated from the bytes of the chunk_type and chunk_data
+        // So skip the first 4 bytes (i.e length), and the last 4 bytes (i.e provided CRC)
+        let calculated_crc = calculate_crc(&orig_value[4..orig_value.len() - 4]);
+        if calculated_crc != crc {
+            return Err(Box::new(ChunkError::InvalidCRCValue));
+        }
+
+        Ok(Chunk {
+            length,
+            chunk_type,
+            chunk_data,
+            crc,
+        })
     }
+}
+
+fn calculate_crc(value: &[u8]) -> u32 {
+    // Based off the implementation of
+    // http://www.libpng.org/pub/png/spec/1.2/PNG-CRCAppendix.html
+    let mut crc: u32 = 0xffffffff; // All 1's
+
+    let crc_table = create_crc_table();
+
+    for v in value.iter() {
+        crc = crc_table[(crc as u8 ^ v) as usize] ^ (crc >> 8);
+    }
+
+    crc ^ 0xffffffff
+}
+
+fn create_crc_table() -> [u32; 256] {
+    let mut crc_table = [0; 256];
+
+    for index in 0..crc_table.len() {
+        let mut c = index as u32;
+        for _ in 0..8 {
+            if c & 1 == 1 {
+                c = 0xedb88320 ^ (c >> 1);
+            } else {
+                c = c >> 1;
+            }
+        }
+        crc_table[index] = c;
+    }
+
+    crc_table
 }
 
 #[cfg(test)]
@@ -74,6 +175,8 @@ mod tests {
             .chain(crc.to_be_bytes().iter())
             .copied()
             .collect();
+
+        println!("{:?}", chunk_data);
 
         Chunk::try_from(chunk_data.as_ref()).unwrap()
     }
