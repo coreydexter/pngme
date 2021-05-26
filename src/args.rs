1d28a85d2cffb19ac9db5aab0328dd5a649d6ba5
Corey Dexter <coreyjdexter@gmail.com>
2021-05-26 20:45:45 +1000

    Get arg handling setup

---
+++ src/args.rs
@@ -0,0 +1,53 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+pub struct ApplicationArguments {
+    #[structopt(subcommand)]
+    pub command: Command,
+}
+
+#[derive(StructOpt, Debug)]
+pub enum Command {
+    #[structopt(name = "encode")]
+    Encode(Encode),
+    #[structopt(name = "decode")]
+    Decode(Decode),
+    #[structopt(name = "remove")]
+    Remove(Remove),
+    #[structopt(name = "print")]
+    Print(Print),
+}
+
+#[derive(StructOpt, Debug)]
+pub struct Encode {
+    #[structopt(parse(from_os_str))]
+    pub file_path: PathBuf,
+    pub chunk_type: String,
+    pub message: String,
+    #[structopt(parse(from_os_str))]
+    pub output_file: Option<PathBuf>,
+}
+
+#[derive(StructOpt, Debug)]
+pub struct Decode {
+    #[structopt(parse(from_os_str))]
+    pub file_path: PathBuf,
+    pub chunk_type: String,
+}
+
+#[derive(StructOpt, Debug)]
+pub struct Remove {
+    #[structopt(parse(from_os_str))]
+    pub file_path: PathBuf,
+    pub chunk_type: String,
+    #[structopt(parse(from_os_str))]
+    pub output_file: Option<PathBuf>,
+}
+
+#[derive(StructOpt, Debug)]
+pub struct Print {
+    #[structopt(parse(from_os_str))]
+    pub file_path: PathBuf,
+}
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ mod chunk_type;
 mod commands;
 mod png;
 
+use crate::args::ApplicationArguments;
+use structopt::StructOpt;
+
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() {
-    todo!();
+    let args = ApplicationArguments::from_args();
 }
