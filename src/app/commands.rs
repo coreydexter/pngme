8f6aae268d034127126dfd4150b52763cdc55f45
Corey Dexter <coreyjdexter@gmail.com>
2021-05-29 15:30:20 +1000

    Use the anyhow in the application for prettier errors

--- src/app/commands.rs
+++ src/app/commands.rs
@@ -1,46 +1,63 @@
 use crate::args::{Decode, Encode, Remove};
+use anyhow::Context;
 use lib_pngme::chunk::Chunk;
 use lib_pngme::png::Png;
 
-pub fn execute_encode(args: Encode) {
-    let mut png = Png::from_file(&args.file_path).expect("Failed to load PNG file");
+pub fn execute_encode(args: Encode) -> anyhow::Result<()> {
+    let mut png = Png::from_file(&args.file_path)
+        .with_context(|| format!("Failed to load PNG file {:?}", args.file_path))?;
 
+    let file_path = args.file_path;
     png.append_chunk(Chunk::new(args.chunk_type, args.message.into_bytes()));
 
     if let Some(output_file) = args.output_file {
         println!("Writing out file to {:?}", output_file);
         png.write_file(&output_file)
-            .expect(&format!("Failed to write out file to {:?}", output_file));
+            .with_context(|| format!("Failed to write file {:?}", output_file))
+    } else {
+        println!("Writing out file to {:?}", file_path);
+        png.write_file(&file_path)
+            .with_context(|| format!("Failed to write file {:?}", file_path))
+    }
+}
+
+pub fn execute_decode(args: Decode) -> anyhow::Result<()> {
+    let png = Png::from_file(&args.file_path)
+        .with_context(|| format!("Failed to load PNG file {:?}", args.file_path))?;
+
+    let chunk = png.chunk_by_type(&args.chunk_type);
+
+    match chunk {
+        Some(chunk) => {
+            let data = chunk.data_as_string().with_context(|| {
+                format!(
+                    "Failed to decode message from {} as string",
+                    args.chunk_type
+                )
+            })?;
+            println!("{}", data);
+        }
+        None => {
+            eprintln!("Failed to find a chunk of type {}", args.chunk_type)
+        }
+    };
+
+    Ok(())
+}
+
+pub fn execute_remove(args: Remove) -> anyhow::Result<()> {
+    let mut png = Png::from_file(&args.file_path)
+        .with_context(|| format!("Failed to load PNG file {:?}", args.file_path))?;
+
+    png.remove_chunk(&args.chunk_type)?;
+
+    if let Some(output_file) = args.output_file {
+        println!("Writing out file to {:?}", output_file);
+        png.write_file(&output_file)
+            .with_context(|| format!("Failed to write file {:?}", output_file))
     } else {
         println!("Writing out file to {:?}", args.file_path);
         png.write_file(&args.file_path)
-            .expect(&format!("Failed to write out file to {:?}", args.file_path));
-    }
-}
-
-pub fn execute_decode(args: Decode) {
-    let png = Png::from_file(&args.file_path).expect("Failed to load PNG file");
-
-    if let Some(chunk) = png.chunk_by_type(&args.chunk_type) {
-        println!(
-            "{}",
-            chunk
-                .data_as_string()
-                .expect("Failed to decode message from string")
-        );
-    }
-}
-
-pub fn execute_remove(args: Remove) {
-    let mut png = Png::from_file(&args.file_path).expect("Failed to load PNG file");
-
-    png.remove_chunk(&args.chunk_type);
-
-    if let Some(output_file) = args.output_file {
-        png.write_file(&output_file)
-            .expect(&format!("Failed to write out file to {:?}", output_file));
-    } else {
-        png.write_file(&args.file_path)
-            .expect(&format!("Failed to write out file to {:?}", args.file_path));
+            .with_context(|| format!("Failed to write file {:?}", args.file_path))
     }
 }
--- src/app/pngme_main.rs
+++ src/app/pngme_main.rs
@@ -5,12 +5,12 @@ use crate::args::ApplicationArguments;
 use commands::{execute_decode, execute_encode, execute_remove};
 use structopt::StructOpt;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let args = ApplicationArguments::from_args();
 
     match args.command {
         args::Command::Encode(args) => execute_encode(args),
         args::Command::Decode(args) => execute_decode(args),
         args::Command::Remove(args) => execute_remove(args),
-    };
+    }
 }
